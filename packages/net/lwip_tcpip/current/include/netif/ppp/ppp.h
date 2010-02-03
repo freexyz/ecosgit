7ac44bac4537ef39eedf46578278c23c91aa02a2
T.C. Chiu <freexyz2007@gmail.com>
2010-02-03 19:35:35 +0800

    2010-02-03 checkout from eCos CVS repository <anoncvs@ecos.sourceware.org>

--- packages/net/lwip_tcpip/current/include/netif/ppp/ppp.h
+++ packages/net/lwip_tcpip/current/include/netif/ppp/ppp.h
@@ -7,13 +7,13 @@
 * The authors hereby grant permission to use, copy, modify, distribute,
 * and license this software and its documentation for any purpose, provided
 * that existing copyright notices are retained in all copies and that this
-* notice and the following disclaimer are included verbatim in any 
+* notice and the following disclaimer are included verbatim in any
 * distributions. No written agreement, license, or royalty fee is required
 * for any of the authorized uses.
 *
 * THIS SOFTWARE IS PROVIDED BY THE CONTRIBUTORS *AS IS* AND ANY EXPRESS OR
 * IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
-* OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. 
+* OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
 * IN NO EVENT SHALL THE CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
 * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT
 * NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
@@ -28,7 +28,7 @@
 * 03-01-01 Marc Boucher <marc@mbsi.ca>
 *   Ported to lwIP.
 * 97-11-05 Guy Lancaster <glanca@gesn.com>, Global Election Systems Inc.
-*	Original derived from BSD codes.
+*   Original derived from BSD codes.
 *****************************************************************************/
 
 #ifndef PPP_H
@@ -36,13 +36,16 @@
 
 #include "lwip/opt.h"
 
-#if PPP_SUPPORT > 0
+#if PPP_SUPPORT /* don't build if not configured for use in lwipopts.h */
+
+#include "lwip/def.h"
 #include "lwip/sio.h"
 #include "lwip/api.h"
 #include "lwip/sockets.h"
 #include "lwip/stats.h"
 #include "lwip/mem.h"
 #include "lwip/tcpip.h"
+#include "lwip/pbuf.h"
 #include "lwip/netif.h"
 
 /*
@@ -91,19 +94,7 @@
  * OR MODIFICATIONS.
  */
 
-#define TIMEOUT(f, a, t)    sys_untimeout((f), (a)), sys_timeout((t)*1000, (f), (a))
-#define UNTIMEOUT(f, a)     sys_untimeout((f), (a))
-
-
-# ifndef __u_char_defined
-
-/* Type definitions for BSD code. */
-typedef unsigned long u_long;
-typedef unsigned int u_int;
-typedef unsigned short u_short;
-typedef unsigned char u_char;
-
-#endif
+#define PPP_TMR_INTERVAL 100
 
 /*
  * Constants and structures defined by the internet system,
@@ -113,8 +104,8 @@ typedef unsigned char u_char;
 /*
  * The basic PPP frame.
  */
-#define PPP_HDRLEN  4       /* octets for standard ppp header */
-#define PPP_FCSLEN  2       /* octets for FCS */
+#define PPP_HDRLEN      4       /* octets for standard ppp header */
+#define PPP_FCSLEN      2       /* octets for FCS */
 
 
 /*
@@ -146,73 +137,74 @@ typedef unsigned char u_char;
 /*
  * Values for FCS calculations.
  */
-#define PPP_INITFCS 0xffff  /* Initial FCS value */
-#define PPP_GOODFCS 0xf0b8  /* Good final FCS value */
+#define PPP_INITFCS     0xffff  /* Initial FCS value */
+#define PPP_GOODFCS     0xf0b8  /* Good final FCS value */
 #define PPP_FCS(fcs, c) (((fcs) >> 8) ^ fcstab[((fcs) ^ (c)) & 0xff])
 
 /*
  * Extended asyncmap - allows any character to be escaped.
  */
-typedef u_char  ext_accm[32];
+typedef u8_t  ext_accm[32];
 
 /*
  * What to do with network protocol (NP) packets.
  */
-enum NPmode {
-    NPMODE_PASS,        /* pass the packet through */
-    NPMODE_DROP,        /* silently drop the packet */
-    NPMODE_ERROR,       /* return an error */
-    NPMODE_QUEUE        /* save it up for later. */
+enum ppp_np_mode {
+  NPMODE_PASS,        /* pass the packet through */
+  NPMODE_DROP,        /* silently drop the packet */
+  NPMODE_ERROR,       /* return an error */
+  NPMODE_QUEUE        /* save it up for later. */
 };
 
 /*
  * Inline versions of get/put char/short/long.
  * Pointer is advanced; we assume that both arguments
  * are lvalues and will already be in registers.
- * cp MUST be u_char *.
+ * cp MUST be u8_t *.
  */
 #define GETCHAR(c, cp) { \
     (c) = *(cp)++; \
 }
 #define PUTCHAR(c, cp) { \
-    *(cp)++ = (u_char) (c); \
+    *(cp)++ = (u8_t) (c); \
 }
 
 
 #define GETSHORT(s, cp) { \
-    (s) = *(cp)++ << 8; \
-    (s) |= *(cp)++; \
+    (s) = *(cp); (cp)++; (s) <<= 8; \
+    (s) |= *(cp); (cp)++; \
 }
 #define PUTSHORT(s, cp) { \
-    *(cp)++ = (u_char) ((s) >> 8); \
-    *(cp)++ = (u_char) (s); \
+    *(cp)++ = (u8_t) ((s) >> 8); \
+    *(cp)++ = (u8_t) (s & 0xff); \
 }
 
 #define GETLONG(l, cp) { \
-    (l) = *(cp)++ << 8; \
-    (l) |= *(cp)++; (l) <<= 8; \
-    (l) |= *(cp)++; (l) <<= 8; \
-    (l) |= *(cp)++; \
+    (l) = *(cp); (cp)++; (l) <<= 8; \
+    (l) |= *(cp); (cp)++; (l) <<= 8; \
+    (l) |= *(cp); (cp)++; (l) <<= 8; \
+    (l) |= *(cp); (cp)++; \
 }
 #define PUTLONG(l, cp) { \
-    *(cp)++ = (u_char) ((l) >> 24); \
-    *(cp)++ = (u_char) ((l) >> 16); \
-    *(cp)++ = (u_char) ((l) >> 8); \
-    *(cp)++ = (u_char) (l); \
+    *(cp)++ = (u8_t) ((l) >> 24); \
+    *(cp)++ = (u8_t) ((l) >> 16); \
+    *(cp)++ = (u8_t) ((l) >> 8); \
+    *(cp)++ = (u8_t) (l); \
 }
 
 
 #define INCPTR(n, cp)   ((cp) += (n))
 #define DECPTR(n, cp)   ((cp) -= (n))
 
-#define BCMP(s0, s1, l)     memcmp((u_char *)(s0), (u_char *)(s1), (l))
-#define BCOPY(s, d, l)      memcpy((d), (s), (l))
+#define BCMP(s0, s1, l)     memcmp((u8_t *)(s0), (u8_t *)(s1), (l))
+#define BCOPY(s, d, l)      MEMCPY((d), (s), (l))
 #define BZERO(s, n)         memset(s, 0, n)
+
 #if PPP_DEBUG
 #define PRINTMSG(m, l)  { m[l] = '\0'; ppp_trace(LOG_INFO, "Remote message: %s\n", m); }
-#else
+#else  /* PPP_DEBUG */
 #define PRINTMSG(m, l)
-#endif
+#endif /* PPP_DEBUG */
 
 /*
  * MAKEHEADER - Add PPP Header fields to a packet.
@@ -227,15 +219,15 @@ enum NPmode {
 *************************/
 
 /* Error codes. */
-#define PPPERR_NONE 0				/* No error. */
-#define PPPERR_PARAM -1				/* Invalid parameter. */
-#define PPPERR_OPEN -2				/* Unable to open PPP session. */
-#define PPPERR_DEVICE -3			/* Invalid I/O device for PPP. */
-#define PPPERR_ALLOC -4				/* Unable to allocate resources. */
-#define PPPERR_USER -5				/* User interrupt. */
-#define PPPERR_CONNECT -6			/* Connection lost. */
-#define PPPERR_AUTHFAIL -7			/* Failed authentication challenge. */
-#define PPPERR_PROTOCOL -8			/* Failed to meet protocol. */
+#define PPPERR_NONE      0 /* No error. */
+#define PPPERR_PARAM    -1 /* Invalid parameter. */
+#define PPPERR_OPEN     -2 /* Unable to open PPP session. */
+#define PPPERR_DEVICE   -3 /* Invalid I/O device for PPP. */
+#define PPPERR_ALLOC    -4 /* Unable to allocate resources. */
+#define PPPERR_USER     -5 /* User interrupt. */
+#define PPPERR_CONNECT  -6 /* Connection lost. */
+#define PPPERR_AUTHFAIL -7 /* Failed authentication challenge. */
+#define PPPERR_PROTOCOL -8 /* Failed to meet protocol. */
 
 /*
  * PPP IOCTL commands.
@@ -244,10 +236,10 @@ enum NPmode {
  * Get the up status - 0 for down, non-zero for up.  The argument must
  * point to an int.
  */
-#define PPPCTLG_UPSTATUS 100	/* Get the up status - 0 down else up */
-#define PPPCTLS_ERRCODE 101		/* Set the error code */
-#define PPPCTLG_ERRCODE 102		/* Get the error code */
-#define	PPPCTLG_FD		103		/* Get the fd associated with the ppp */
+#define PPPCTLG_UPSTATUS 100 /* Get the up status - 0 down else up */
+#define PPPCTLS_ERRCODE  101 /* Set the error code */
+#define PPPCTLG_ERRCODE  102 /* Get the error code */
+#define PPPCTLG_FD       103 /* Get the fd associated with the ppp */
 
 /************************
 *** PUBLIC DATA TYPES ***
@@ -258,38 +250,37 @@ enum NPmode {
  * for a particular protocol.
  */
 struct protent {
-    u_short protocol;       /* PPP protocol number */
-    /* Initialization procedure */
-    void (*init) (int unit);
-    /* Process a received packet */
-    void (*input) (int unit, u_char *pkt, int len);
-    /* Process a received protocol-reject */
-    void (*protrej) (int unit);
-    /* Lower layer has come up */
-    void (*lowerup) (int unit);
-    /* Lower layer has gone down */
-    void (*lowerdown) (int unit);
-    /* Open the protocol */
-    void (*open) (int unit);
-    /* Close the protocol */
-    void (*close) (int unit, char *reason);
+  u16_t protocol;           /* PPP protocol number */
+  /* Initialization procedure */
+  void (*init) (int unit);
+  /* Process a received packet */
+  void (*input) (int unit, u8_t *pkt, int len);
+  /* Process a received protocol-reject */
+  void (*protrej) (int unit);
+  /* Lower layer has come up */
+  void (*lowerup) (int unit);
+  /* Lower layer has gone down */
+  void (*lowerdown) (int unit);
+  /* Open the protocol */
+  void (*open) (int unit);
+  /* Close the protocol */
+  void (*close) (int unit, char *reason);
 #if 0
-    /* Print a packet in readable form */
-    int  (*printpkt) (u_char *pkt, int len,
-              void (*printer) (void *, char *, ...),
-              void *arg);
-    /* Process a received data packet */
-    void (*datainput) (int unit, u_char *pkt, int len);
+  /* Print a packet in readable form */
+  int  (*printpkt) (u8_t *pkt, int len, void (*printer) (void *, char *, ...),
+                    void *arg);
+  /* Process a received data packet */
+  void (*datainput) (int unit, u8_t *pkt, int len);
 #endif
-    int  enabled_flag;      /* 0 iff protocol is disabled */
-    char *name;         /* Text name of protocol */
+  int enabled_flag;         /* 0 if protocol is disabled */
+  char *name;               /* Text name of protocol */
 #if 0
-    /* Check requested options, assign defaults */
-    void (*check_options) (u_long);
-    /* Configure interface for demand-dial */
-    int  (*demand_conf) (int unit);
-    /* Say whether to bring up link for this pkt */
-    int  (*active_pkt) (u_char *pkt, int len);
+  /* Check requested options, assign defaults */
+  void (*check_options) (u32_t);
+  /* Configure interface for demand-dial */
+  int  (*demand_conf) (int unit);
+  /* Say whether to bring up link for this pkt */
+  int  (*active_pkt) (u8_t *pkt, int len);
 #endif
 };
 
@@ -298,50 +289,59 @@ struct protent {
  * the last NP packet was sent or received.
  */
 struct ppp_idle {
-    u_short xmit_idle;      /* seconds since last NP packet sent */
-    u_short recv_idle;      /* seconds since last NP packet received */
+  u16_t xmit_idle;      /* seconds since last NP packet sent */
+  u16_t recv_idle;      /* seconds since last NP packet received */
 };
 
 struct ppp_settings {
+  u32_t disable_defaultip : 1;          /* Don't use hostname for default IP addrs */
+  u32_t auth_required     : 1;          /* Peer is required to authenticate */
+  u32_t explicit_remote   : 1;          /* remote_name specified with remotename opt */
+  u32_t refuse_pap        : 1;          /* Don't wanna auth. ourselves with PAP */
+  u32_t refuse_chap       : 1;          /* Don't wanna auth. ourselves with CHAP */
+  u32_t usehostname       : 1;          /* Use hostname for our_name */
+  u32_t usepeerdns        : 1;          /* Ask peer for DNS adds */
 
-	u_int  disable_defaultip : 1;   /* Don't use hostname for default IP addrs */
-	u_int  auth_required : 1;      /* Peer is required to authenticate */
-	u_int  explicit_remote : 1;    /* remote_name specified with remotename opt */
-	u_int  refuse_pap : 1;         /* Don't wanna auth. ourselves with PAP */
-	u_int  refuse_chap : 1;        /* Don't wanna auth. ourselves with CHAP */
-	u_int  usehostname : 1;        /* Use hostname for our_name */
-	u_int  usepeerdns : 1;         /* Ask peer for DNS adds */
+  u16_t idle_time_limit;                /* Shut down link if idle for this long */
+  int maxconnect;                       /* Maximum connect time (seconds) */
 
-	u_short idle_time_limit; /* Shut down link if idle for this long */
-	int  maxconnect;         /* Maximum connect time (seconds) */
-
-	char user[MAXNAMELEN + 1];/* Username for PAP */
-	char passwd[MAXSECRETLEN + 1];           /* Password for PAP, secret for CHAP */
-	char our_name[MAXNAMELEN + 1];         /* Our name for authentication purposes */
-	char remote_name[MAXNAMELEN + 1];      /* Peer's name for authentication */
+  char user       [MAXNAMELEN   + 1];   /* Username for PAP */
+  char passwd     [MAXSECRETLEN + 1];   /* Password for PAP, secret for CHAP */
+  char our_name   [MAXNAMELEN   + 1];   /* Our name for authentication purposes */
+  char remote_name[MAXNAMELEN   + 1];   /* Peer's name for authentication */
 };
 
 struct ppp_addrs {
-    struct ip_addr our_ipaddr, his_ipaddr, netmask, dns1, dns2;
+  struct ip_addr our_ipaddr, his_ipaddr, netmask, dns1, dns2;
 };
 
 /*****************************
 *** PUBLIC DATA STRUCTURES ***
 *****************************/
+
 /* Buffers for outgoing packets. */
-extern u_char outpacket_buf[NUM_PPP][PPP_MRU+PPP_HDRLEN];
+extern u8_t *outpacket_buf[NUM_PPP];
 
 extern struct ppp_settings ppp_settings;
 
-extern struct protent *ppp_protocols[];/* Table of pointers to supported protocols */
+extern struct protent *ppp_protocols[]; /* Table of pointers to supported protocols */
 
 
 /***********************
 *** PUBLIC FUNCTIONS ***
 ***********************/
 
+/* PPP link status callback */
+typedef void (*ppp_status_cb_t)(void *ctx, int err, void *arg);
+
+
 /* Initialize the PPP subsystem. */
-void pppInit(void);
+err_t ppp_init(void);
+
+/**
+ * This should be called every PPP_TMR_INTERVAL milliseconds.
+ */
+void ppp_tmr(void);
 
 /* Warning: Using PPPAUTHTYPE_ANY might have security consequences.
  * RFC 1994 says:
@@ -362,55 +362,79 @@ void pppInit(void);
  * which identifies exactly one authentication method.
  *
  */
-enum pppAuthType {
+enum ppp_auth_type {
     PPPAUTHTYPE_NONE,
     PPPAUTHTYPE_ANY,
     PPPAUTHTYPE_PAP,
     PPPAUTHTYPE_CHAP
 };
 
-void pppSetAuth(enum pppAuthType authType, const char *user, const char *passwd);
+void ppp_set_auth(enum ppp_auth_type auth_type, const char *user, const char *passwd);
 
 /*
- * Open a new PPP connection using the given I/O device.
+ * Open a new PPP connection using the given serial I/O device.
  * This initializes the PPP control block but does not
  * attempt to negotiate the LCP session.
  * Return a new PPP connection descriptor on success or
- * an error code (negative) on failure. 
+ * an error code (negative) on failure.
  */
-int pppOpen(sio_fd_t fd, void (*linkStatusCB)(void *ctx, int errCode, void *arg), void *linkStatusCtx);
+int ppp_open_serial(sio_fd_t fd, ppp_status_cb_t status_cb, void *status_ctx);
+
 
 /*
- * Close a PPP connection and release the descriptor. 
- * Any outstanding packets in the queues are dropped.
- * Return 0 on success, an error code on failure. 
+ * Open a new PPP Over Ethernet (PPPOE) connection.
  */
-int pppClose(int pd);
+int ppp_open_ethernet(struct netif *ethif,
+                      const char *service_name, const char *concentrator_name,
+                      ppp_status_cb_t status_cb, void *status_ctx);
+
+
+void ppp_poll(int pd);
+
+/*
+ * Close a PPP connection and release the descriptor.
+ * Any outstanding packets in the queues are dropped.
+ * Return 0 on success, an error code on failure.
+ */
+int ppp_close(int pd);
+
+/*
+ * Check if the PPP connection is open.
+ */
+int ppp_is_open(int pd);
 
 /*
  * Indicate to the PPP process that the line has disconnected.
  */
-void pppSigHUP(int pd);
+void ppp_sig_hup(int pd);
 
 /*
  * Get and set parameters for the given connection.
- * Return 0 on success, an error code on failure. 
+ * Return 0 on success, an error code on failure.
  */
-int  pppIOCtl(int pd, int cmd, void *arg);
+int ppp_ioctl(int pd, int cmd, void *arg);
 
 /*
  * Return the Maximum Transmission Unit for the given PPP connection.
  */
-u_int pppMTU(int pd);
+u32_t ppp_mtu(int pd);
 
 /*
  * Write n characters to a ppp link.
- *	RETURN: >= 0 Number of characters written
- *		 	 -1 Failed to write to device
+ * RETURN: >= 0 Number of characters written, -1 Failed to write to device.
  */
-int pppWrite(int pd, const u_char *s, int n);
+int ppp_write(int pd, const u8_t *s, int n);
 
-void pppMainWakeup(int pd);
+void ppp_in_proc_over_ethernet(int pd, struct pbuf *pb);
+
+struct pbuf *ppp_single_buf(struct pbuf *p);
+
+void ppp_link_terminated(int pd);
+
+void ppp_link_down(int pd);
+
+void ppp_main_wakeup(int pd);
+
 
 /* Configure i/f transmit parameters */
 void ppp_send_config (int, int, u32_t, int, int);
@@ -424,11 +448,11 @@ int  get_idle_time (int, struct ppp_idle *);
 /* Configure VJ TCP header compression */
 int  sifvjcomp (int, int, int, int);
 /* Configure i/f down (for IP) */
-int  sifup (int);		
+int  sifup (int);
 /* Set mode for handling packets for proto */
-int  sifnpmode (int u, int proto, enum NPmode mode);
+int  sifnpmode (int u, int proto, enum ppp_np_mode mode);
 /* Configure i/f down (for IP) */
-int  sifdown (int);	
+int  sifdown (int);
 /* Configure IP addresses for i/f */
 int  sifaddr (int, u32_t, u32_t, u32_t, u32_t, u32_t);
 /* Reset i/f IP addresses */
@@ -439,7 +463,7 @@ int  sifdefaultroute (int, u32_t, u32_t);
 int  cifdefaultroute (int, u32_t, u32_t);
 
 /* Get appropriate netmask for address */
-u32_t GetMask (u32_t); 
+u32_t GetMask (u32_t);
 
 #endif /* PPP_SUPPORT */
 
